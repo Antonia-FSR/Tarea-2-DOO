1c0e4fa5c6ef2faa3c5f94c75c859bd161343292
jaocSec <jaoc2001@hotmail.com>
2026-05-22 18:46:26 -0400

    Refactor: Implementados pruebas para verificar funcionamiento de los cambios hechos a la clase

--- src/test/java/org/example/RetrasoTest.java
+++ src/test/java/org/example/RetrasoTest.java
@@ -8,15 +8,17 @@ class RetrasoTest {
     @Test
     void testCreacionYGetter() {
         // se prueba que al generar el retraso se guarde la hora exacta
+        Empleado empleado= new Empleado("1", "Perez", "Juan", "juan@gmail.com");
         Instant momentoActual = Instant.now();
-        Retraso retraso = new Retraso(momentoActual);
+        Retraso retraso = new Retraso(empleado, momentoActual);
 
         assertEquals(momentoActual, retraso.getHora());
     }
     @Test
     void testSetter() {
         // se prueba que al hacer los cambios se guarden
-        Retraso retraso = new Retraso(Instant.now());
+        Empleado empleado= new Empleado("2", "Gomez", "Ana", "ana@gmail.com");
+        Retraso retraso = new Retraso(empleado, Instant.now());
 
         // una hora inventada para probar el cambio
         Instant nuevaHora = Instant.parse("2031-07-09T10:15:30Z");
@@ -27,9 +29,10 @@ class RetrasoTest {
 
     @Test
     void testToString() {
+        Empleado empleado = new Empleado("3", "Torres", "Luis", "luis@gmail.com");
         // se prueba que la informacion al imprimir sea la correcta
         Instant horaFija = Instant.parse("1984-10-02T11:18:41Z");
-        Retraso retraso = new Retraso(horaFija);
+        Retraso retraso = new Retraso(empleado, horaFija);
 
         assertTrue(retraso.toString().contains("1984-10-02T11:18:41Z"));
     }
